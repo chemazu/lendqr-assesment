5ee09332237d77736de7f0c01d8db8883b220187
chemazu <chukwuemekachemazu@gmail.com>
2022-10-06 06:01:34 +0100

    left section of user detail page completed

--- src/Views/UserDetail/index.tsx
+++ src/Views/UserDetail/index.tsx
@@ -7,11 +7,57 @@ import users from "../../assests/img/users.svg";
 import briefcase from "../../assests/img/briefcase.svg";
 import arrowDown from "../../assests/img/arrow-down.svg";
 import dash from "../../assests/img/dash.svg";
-
 import avatar from "../../assests/img/avatar.svg";
-import "./style.scss";
+import customer2 from "../../assests/img/customer-2.svg";
+import customer3 from "../../assests/img/customer-3.svg";
+import customer4 from "../../assests/img/customer-4.svg";
+import customer5 from "../../assests/img/customer-5.svg";
+import customer6 from "../../assests/img/customer-6.svg";
+import customer7 from "../../assests/img/customer-7.svg";
+import customer8 from "../../assests/img/customer-8.svg";
+import business3 from "../../assests/img/business-3.svg";
+import business4 from "../../assests/img/business-4.svg";
+import business5 from "../../assests/img/business-5.svg";
+import business6 from "../../assests/img/business-6.svg";
+import business7 from "../../assests/img/business-7.svg";
+import business8 from "../../assests/img/business-8.svg";
+import business9 from "../../assests/img/business-9.svg";
+import logOutSvg from "../../assests/img/log-out.svg";
+import set1 from "../../assests/img/set-1.svg";
+
+import set2 from "../../assests/img/set-2.svg";
+import set3 from "../../assests/img/set-3.svg";
+import set4 from "../../assests/img/customer-4.svg";
 
 export default function UserDetail() {
+  let customers = [
+    { title: "Users", img: users },
+    { title: "Guarantors", img: customer2 },
+    { title: "Loans", img: customer3 },
+    { title: "Decision Models", img: customer4 },
+    { title: "Savings", img: customer5 },
+    { title: "Loan Requests", img: customer6 },
+    { title: "Whitelist", img: customer7 },
+    { title: "Karma", img: customer8 },
+  ];
+  let businesses = [
+    { title: "Organization", img: briefcase },
+    { title: "Loan Products", img: customer6 },
+    { title: "Savings Products", img: business3 },
+    { title: "Fees and Charges", img: business4 },
+    { title: "Transactions", img: business5 },
+    { title: "Services", img: business6 },
+    { title: "Service Account", img: business7 },
+    { title: "Settlements", img: business8 },
+    { title: "Reports", img: business9 },
+  ];
+  let settings = [
+    { title: "Preferences", img: set1 },
+    { title: "Fees and Pricing", img: set2 },
+    { title: "Audit Logs", img: set3 },
+    { title: "Systems Messages", img: set4 },
+    { title: "Logout", img: logOutSvg },
+  ];
   return (
     <div className="user-detail">
       <div className="left">
@@ -28,71 +74,46 @@ export default function UserDetail() {
         <div className="customers">
           <div className="top">
             <h4>Customers</h4>
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>
+          </div>
+          <div className="bottom">
+            {customers.map((item: { title: string; img: any }, index) => {
+              return (
+                <div className="left-item" key={index}>
+                  <img src={item.img} alt="users" />
+                  <p>{item.title}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
-        <div className="customers">
+        <div className="businesses">
           <div className="top">
-            <h4>Customers</h4>
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>{" "}
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>
-            <div className="left-item">
-              <img src={users} alt="users" />
-              <p>Users</p>
-            </div>
+            <h4>Businesses</h4>
+          </div>
+          <div className="bottom">
+            {businesses.map((item: { title: string; img: any }, index) => {
+              return (
+                <div className="left-item" key={index}>
+                  <img src={item.img} alt="users" />
+                  <p>{item.title}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="settings">
+          <div className="top">
+            <h4>Settings</h4>
+          </div>
+          <div className="bottom">
+            {settings.map((item: { title: string; img: any }, index) => {
+              return (
+                <div className="left-item" key={index}>
+                  <img src={item.img} alt="users" />
+                  <p>{item.title}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
