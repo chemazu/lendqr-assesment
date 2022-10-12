f52d78dd042e8a6e7b4394ed1b6e56b8f14be281
chemazu <chukwuemekachemazu@gmail.com>
2022-10-12 10:28:39 +0100

    implemented tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { MemoryRouter } from "react-router-dom";
-import { findByRole, render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
 import Login from "../src/Views/Login";
 import Dashboard from "./Views/Dashboard";
 import AuthProvider from "../src/context/AuthContext";
+import UserDetail from "./Views/UserDetail";
 
 test("Login", () => {
   render(
@@ -11,8 +12,8 @@ test("Login", () => {
       <Login />
     </MemoryRouter>
   );
-  const linkElement = screen.getByText(/LOG IN/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginText = screen.getByText(/LOG IN/i);
+  expect(loginText).toBeInTheDocument();
 });
 test("Dashboard", async () => {
   render(
@@ -22,8 +23,38 @@ test("Dashboard", async () => {
       </MemoryRouter>
     </AuthProvider>
   );
-  const linkElement = screen.getAllByText(/Loading.../i);
-  expect(linkElement[0]).toBeInTheDocument();
+  const loadingElement = screen.getAllByText(/Loading.../i);
+  expect(loadingElement[0]).toBeInTheDocument();
   let userList = await screen.findByRole("table");
   expect(userList).toBeInTheDocument();
 });
+// test("UserDetail", async () => {
+//   render(
+//     <AuthProvider>
+//       <MemoryRouter>
+//         <Routes>
+//           <Route path="/user/1" element={<UserDetail />} />
+//         </Routes>
+//       </MemoryRouter>
+//     </AuthProvider>
+//   );
+
+//   // element is initially not present...
+
+//   // wait for appearance inside an assertion
+//   // let userList = await screen.findByRole("table");
+//   const fullname =   screen.getAllByText(/User Details/i);
+//   expect(fullname).toBeInTheDocument();
+
+
+//   // await waitFor(() => {
+//   //   const fullname = screen.getByText(/full/i);
+
+//   //   expect(fullname).toBeInTheDocument();
+//   // });
+
+//   // const linkElement = await screen.getAllByText(/FULL NAME/i);
+//   // expect(linkElement[0]).toBeInTheDocument();
+//   // let userList = await screen.findByRole("table");
+//   // expect(userList).toBeInTheDocument();
+// });
